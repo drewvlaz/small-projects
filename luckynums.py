6a5063e2d579bfc3587ef4c28eee4e6f5ad97a63
drewvlaz <314dsv@gmail.com>
2018-10-19 20:16:19 -0400

    Updated luckynums

--- luckynums.py
+++ luckynums.py
@@ -4,9 +4,11 @@ import numpy as np
 def get_lucky_nums(max_num, max_pb, num_of_nums=5):
     """ Print a random set of numbers and a powerball """
 
-    # Calculate random numbers in given range 
-    nums =  np.random.randint(1, max_num + 1, size=num_of_nums)
-    powerball = np.random.randint(1, max_pb + 1, size=1)
+    # Calculate random numbers in given range after scrambling a random amount of times
+    times = np.random.randint(1,11)
+    for i in range(times):
+        nums =  np.random.randint(1, max_num + 1, size=num_of_nums)
+        powerball = np.random.randint(1, max_pb + 1, size=1)
 
     # Replace any duplicates
     final = []
@@ -25,9 +27,9 @@ def get_lucky_nums(max_num, max_pb, num_of_nums=5):
     # Convert final from list to numpy array
     final = np.asarray(final)
 
-    # Print results
-    print(f"Your final numbers are now: {final}")
-    print(f"The Powerball is: {powerball[0]}")
+    # Print result
+    print(f"Your numbers are: {final}")
+    print(f"Powerball: {powerball[0]}")
 
 
 get_lucky_nums(70, 25)
