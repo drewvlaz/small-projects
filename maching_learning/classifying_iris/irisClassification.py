530893c68413f5137e64a8d78187da22c01e623a
drewvlaz <314dsv@gmail.com>
2018-11-11 21:07:13 -0500

    Added machine_learning

---
+++ maching_learning/classifying_iris/irisClassification.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import sqlite3
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+connection = sqlite3.connect('data/iris/database.sqlite')
+data = pd.read_sql_query('SELECT * FROM Iris', connection)
+
+# print(data['Species'].value_counts())
+
+# remove redundant column
+del data['Id']
+
+# create list of features to train on
+features = []
+for col in list(data.columns):
+    if 'Species' not in col:
+        features.append(col)
+
+
+# create the features and target datasets
+X = data[features]
+y = data['Species']
+
+# one-hot encode
+y = pd.get_dummies(y)
+
+# split into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=324)
+
+# determine regression type and create model
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# use model to predict flower type and print the root mean squared error
+y_prediction = regressor.predict(X_test)
+RMSE = sqrt(mean_squared_error(y_true = y_test, y_pred = y_prediction))
+print("Linear Regression RMSE:",RMSE)
+
+# determine regression type and create model
+regressor = DecisionTreeRegressor(max_depth=25)
+regressor.fit(X_train, y_train)
+
+# use model to predict the flower type and print the root mean squared error
+y_prediction = regressor.predict(X_test)
+y_prediction
+
+RMSE = sqrt(mean_squared_error(y_true = y_test, y_pred = y_prediction))
+print("Decision Tree Regressor RMSE:",RMSE)
